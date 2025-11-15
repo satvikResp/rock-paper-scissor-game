c81564af3541ba6294c623932452cced10a8d892
satvikResp <satvikgautam5@gmail.com>
2025-11-15 08:49:14 +0530

    Add initial HTML structure for Rock Paper Scissors

---
+++ javas.css
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Rock Paper Scissors</title>
+  <link rel="stylesheet" href="javas.css" />
+</head>
+<body>
+  <div class="container">
+    <h1>Rock Paper Scissors </h1>
+    <div class="imagediv">
+      <button id="btn1"></button>
+      <button id="btn2"></button>
+      <button id="btn3"></button>
+    </div>
+
+    <div class="scorediv">
+      <div class="you">
+        <div>You</div>
+        <div id="userSc">0</div>
+      </div>
+      <div class="computer">
+        <div>Computer</div>
+        <div id="compSc">0</div>
+      </div>
+    </div>
+
+    <div class="result" id="roundResult"></div>
+    <div class="final-result" id="finalResult"></div>
+    <div class="sub-result" id="subResult"></div>
+    <div class="refresh" onclick="window.location.reload()">&#8635;</div>
+  </div>
+  <script src="first.js"></script>
+</body>
+</html>
